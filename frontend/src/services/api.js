dbf5b8a5d331f53001da88016abb4d88dcb31e28
Kiruthiga012 <kiruthigav.24it@kongu.edu>
2026-06-17 11:34:32 +0530

    Merged remote changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/atm";
+const API = "https://atmapplication-4e0r.onrender.com";
 
 export const signup = (data) => {
   return axios.post(`${API}/signup`, data);
