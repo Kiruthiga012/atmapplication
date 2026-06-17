d8672d4ebed75feeb50daf860b85ba85d2fa29b0
Kiruthiga012 <kiruthigav.24it@kongu.edu>
2026-06-17 11:38:33 +0530

    Merged remote changes

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://atmapplication-rcrq.vercel.app",
+  })
+);
+
 app.use(express.json());
+
+// app.use("/api/atm", require("./routes/atmRoutes"));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -20,8 +27,7 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
-app.use("/api/atm", require("./routes/atmRoutes"));
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Server Running");
+  console.log(`Server Running on Port ${process.env.PORT || 5000}`);
 });
