df6336db887f67010b9e112457c9937e7c06d293
Kiruthiga012 <kiruthigav.24it@kongu.edu>
2026-06-17 11:47:06 +0530

    Merged remote changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB Connection Error:", error.message);
-    process.exit(1);
-  }
+const connectDB=async()=>{
+    try{
+        console.log("URI =",process.env.MONGO_URL);
+        
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB connected"+process.env.PORT);
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
-
 module.exports = connectDB;
